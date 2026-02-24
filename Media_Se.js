aaf84bb04d72accbd0e8060f5742c7f0a6f44bfa
nicolly1265 <nicolly.santos.ribeiro@escola.pr.gov.br>
2026-02-24 09:48:15 -0300

    Add files via upload

---
+++ Media_Se.js
@@ -0,0 +1,14 @@
+let aluno= "Robson";
+let N1=99;
+let N2=100;
+let N3=99;
+let N4=100;
+let media=0;
+let result="";
+media=(N1+N2+N3+N4)/4;
+if(media >=70){
+result="Aprovado"
+}else{
+result="Reprovado"
+}
+console.log("O aluno"+aluno+"tem a media"+media+"e está:"+result);
